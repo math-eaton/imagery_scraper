e7bedb9b8b5dcff07aac992e5aeb1d6d2803a0f7
math-eaton <mheaton@ciesin.columbia.edu>
2023-08-05 03:00:19 -0400

    Revert "data cleaning"
    This reverts commit 18eb9a7951ed1a8ec84778276d791395e9119c91.

--- transmissionConvexHull.py
+++ transmissionConvexHull.py
@@ -1,32 +1,19 @@
 import pandas as pd
-import folium
+import matplotlib.pyplot as plt
 
-dtype = {
-    'application_id': str,
-    'service': str,
-    'lms_application_id': str,
-    'dts_site_number': str,
-}
+# load the data
+df = pd.read_csv('data/fm_contours_sample.csv') # replace with your file path
 
-# add latitude and longitude fields to dtype
-for i in range(1, 362):
-    dtype[f'latitude_{i}'] = float
-    dtype[f'longitude_{i}'] = float
+plt.figure(figsize=(10,10))
 
-# read only necessary columns
-necessary_columns = list(dtype.keys())
-df = pd.read_csv('data/FM_service_contour_processed.csv', delimiter=',', dtype=dtype, usecols=necessary_columns)
-
-df['application_id'] = pd.to_numeric(df['application_id'], errors='coerce')
-df['dts_site_number'] = pd.to_numeric(df['dts_site_number'], errors='coerce')
-
-# map instance
-m = folium.Map(location=[45.5236, -122.6750], zoom_start=2)
-
+# loop through rows and plot each journey as a separate line
 for _, row in df.iterrows():
-    latitudes = row[[f'latitude_{i}' for i in range(1,362)]].values
-    longitudes = row[[f'longitude_{i}' for i in range(1,362)]].values
-    locations = list(zip(latitudes, longitudes))
-    folium.PolyLine(locations=locations, color='blue', weight=2.5, opacity=1).add_to(m)
+    # get columns named '0', '1', ..., '359' and drop missing values
+    coordinates = row[[str(i) for i in range(360)]].dropna()
+    # convert strings "latitude, longitude" into list of [latitude, longitude]
+    coordinates = [coord.split(',') for coord in coordinates]
+    # transpose list of coordinates into two lists: latitudes and longitudes
+    latitudes, longitudes = map(list, zip(*((float(coord[0]), float(coord[1])) for coord in coordinates)))
+    plt.plot(longitudes, latitudes)
 
-m.save('output/folium/output.html') 
+plt.show()
