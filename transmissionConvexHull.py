f135ce380e2c43cba1ef5ad17094bef85da422e6
math-eaton <mheaton@ciesin.columbia.edu>
2023-08-05 03:43:33 -0400

    styling

--- transmissionConvexHull.py
+++ transmissionConvexHull.py
@@ -10,7 +10,7 @@ def simplify_polyline(coordinates, tolerance=0.01):
     return list(simplified_line.coords)
 
 # Create a Map instance
-m = folium.Map(location=[45.5236, -122.6750], zoom_start=2)
+m = folium.Map(location=[37.0902, -95.7129], zoom_start=5, tiles='CartoDB Positron')
 
 # Specify the chunk size for reading the CSV
 chunksize = 1000
@@ -20,7 +20,7 @@ total_rows = sum(1 for _ in open('data/FM_service_contour_current.csv'))  # Coun
 total_chunks = total_rows // chunksize + 1  # Calculate the total number of chunks
 
 # Load the data in chunks and track progress with tqdm
-for chunk_idx, df_chunk in tqdm(enumerate(pd.read_csv('data/FM_service_contour_current.csv', chunksize=chunksize, nrows=5000)), total=total_chunks, desc='Total CSV'):
+for chunk_idx, df_chunk in tqdm(enumerate(pd.read_csv('data/FM_service_contour_current.csv', chunksize=chunksize, nrows=2000)), total=total_chunks, desc='Total CSV'):
     chunk_rows = len(df_chunk)  # Number of rows in the current chunk
     for _, row in tqdm(df_chunk.iterrows(), total=chunk_rows, desc='Processing Chunk', leave=False):
         # get columns named '0', '1', ..., '359' and drop missing values
@@ -46,7 +46,7 @@ for chunk_idx, df_chunk in tqdm(enumerate(pd.read_csv('data/FM_service_contour_c
         # Check if simplified_locations is empty before creating a PolyLine
         if simplified_locations:
             # Create a PolyLine with the simplified coordinates:
-            folium.PolyLine(locations=simplified_locations, color='blue', weight=2.5, opacity=1).add_to(m)
+            folium.PolyLine(locations=simplified_locations, color='#0c13e5', weight=1, opacity=0.7).add_to(m)
 
 # Save it as html
 m.save('output/folium/output.html')
