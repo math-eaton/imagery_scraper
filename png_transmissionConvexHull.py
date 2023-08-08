41c5ca7c1df7dfb56843f0ad06a1df2291014a72
math-eaton <mheaton@ciesin.columbia.edu>
2023-08-08 14:46:28 -0400

    fill polyline to edges

--- png_transmissionConvexHull.py
+++ png_transmissionConvexHull.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from shapely.geometry import LineString
 from tqdm import tqdm
+import numpy as np
 
 # Function to simplify a list of coordinates
 def simplify_polyline(coordinates, tolerance=0.01):
@@ -9,15 +10,33 @@ def simplify_polyline(coordinates, tolerance=0.01):
     simplified_line = line.simplify(tolerance, preserve_topology=False)
     return list(simplified_line.coords)
 
+# Function to get the number of lines in a file
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+# Specify the input file path
+input_file_path = 'data/processed/FM_service_contour_current_processed.csv'
+
+# Get the total number of rows in the CSV file
+len_of_file = file_len(input_file_path)
+print(len_of_file)
+
+# Skipping every Nth row
+N = 10  # Change this value to your desired sampling rate
+skipped = np.setdiff1d(np.arange(len_of_file), np.arange(0, len_of_file, N))
+print(skipped)
+
 # Specify the chunk size for reading the CSV
 chunksize = 100
 
-# Get the total number of rows in the CSV file
-total_rows = sum(1 for _ in open('data/FM_service_contour_current.csv'))  # Count rows without reading the entire file
-total_chunks = total_rows // chunksize + 1  # Calculate the total number of chunks
+# Get the total number of chunks
+total_chunks = len(skipped) // chunksize + 1
 
-# Load the data in chunks and track progress with tqdm
-for chunk_idx, df_chunk in tqdm(enumerate(pd.read_csv('data/FM_service_contour_current.csv', chunksize=chunksize, nrows=300)), total=total_chunks, desc='Total CSV'):
+# Read the CSV file with skipped rows and chunking
+for chunk_idx, df_chunk in tqdm(enumerate(pd.read_csv(input_file_path, skiprows=skipped, chunksize=chunksize)), total=total_chunks, desc='Total CSV'):
     chunk_rows = len(df_chunk)  # Number of rows in the current chunk
     for _, row in tqdm(df_chunk.iterrows(), total=chunk_rows, desc='Processing Chunk', leave=False):
         # Get columns named '0', '1', ..., '359' and drop missing values
@@ -49,15 +68,22 @@ for chunk_idx, df_chunk in tqdm(enumerate(pd.read_csv('data/FM_service_contour_c
             xs, ys = zip(*simplified_locations)
             plt.plot(xs, ys, color='blue', linewidth=2)
 
+            buffer = 0.001  # You can adjust this value
+
             # Set the extent of the plot to match the extent of the polyline feature
             min_x, min_y = min(xs), min(ys)
             max_x, max_y = max(xs), max(ys)
-            plt.xlim(min_x, max_x)
-            plt.ylim(min_y, max_y)
+
+            # Add the buffer
+            plt.xlim(min_x - buffer, max_x + buffer)
+            plt.ylim(min_y - buffer, max_y + buffer)
+
+            # Remove axes
+            plt.axis('off')
+
+            # Adjust the figure size to fit the plot
+            plt.tight_layout(pad=0)
 
             # Save the figure as a PNG image with the name based on the application_id
             application_id = row['application_id']
             plt.savefig(f'output/polyline_images/{application_id}.png')
-
-            # Close the figure to free up memory
-            plt.close()
